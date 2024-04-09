8f098ce49cbdafa0e42d8414078d1a9171ff02ca
boben195 <boben195@ukr.net>
2024-04-09 21:06:48 +0300

    add files redux

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const INITIAL_STATE = [
+  {"id": "id-1", "username": "Rosie Simpson", "number": "459-12-56"},
+  {"id": "id-2", "username": "Hermione Kline", "number": "443-89-12"},
+  {"id": "id-3", "username": "Eden Clements", "number": "645-17-79"},
+  {"id": "id-4", "username": "Annie Copeland", "number": "227-91-26"}
+]
+
+const contactsSlice = createSlice({
+    name: 'contacts',
+    initialState: {
+        items: INITIAL_STATE
+    },
+    reducers: {
+        addContact(state, action) {
+            state.items.push(action.payload)
+        },
+        deleteContact(state, action) {
+            state.items = state.items.filter((el => el.id !== action.payload))
+        },
+    },
+})
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+export const selectContacts = state => state.contacts.items;
+export const contactsReducer = contactsSlice.reducer;
